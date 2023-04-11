05acc9b81413dac02960b871e1a0eac3438eccc0
olsontj4 <olsontj4@my.lanecc.edu>
2023-04-11 13:52:34 -0700

    WIP CaesarCypher.

--- StringsLab_Core5/ShiftCypher/Program.cs
+++ StringsLab_Core5/ShiftCypher/Program.cs
@@ -6,7 +6,19 @@ namespace ShiftCypher
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Type a word or phrase:");
+            string sentence = Console.ReadLine();
+            string[] words = sentence.Split();
+            Console.WriteLine("Type a number to shift to:");
+            int shift = int.Parse(Console.ReadLine());
+            for (int i = 0; i < words.Length; i++)
+            {
+                Console.Write(ShiftCypher(words[i], shift) + " ");
+            }
+        }
+        static string ShiftCypher(string sentence, int shift)
+        {
+            return "a";
         }
     }
 }
